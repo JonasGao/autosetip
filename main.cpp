fc8b9f90b2fefad9abab74101a375f13af44f1e0
Jonas Gao <gaopinsong@live.cn>
2024-04-18 19:52:28 +0800

    Init

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <curl/curl.h>
+
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string *) userp)->append((char *) contents, size * nmemb);
+    return size * nmemb;
+}
+
+int main() {
+    CURL *curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    std::cout << "Startup" << std::endl;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.ipify.org");
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perm() failed: %s", curl_easy_strerror(res));
+        } else {
+            std::cout << "IP Address: " << readBuffer << std::endl;
+        }
+        curl_easy_cleanup(curl);
+    } else {
+        fprintf(stderr, "curl_easy_init() failed.");
+    }
+    curl_global_cleanup();
+    return 0;
+}
